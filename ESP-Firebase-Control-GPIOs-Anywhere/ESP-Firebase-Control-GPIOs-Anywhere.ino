2ff83753154ae9b34a8bda6fe89249f7fcc2a263
Rui Santos <randomnerdtutorials@gmail.com>
2025-05-26 17:54:15 +0100

    Update ESP-Firebase-Control-GPIOs-Anywhere.ino

--- ESP-Firebase-Control-GPIOs-Anywhere/ESP-Firebase-Control-GPIOs-Anywhere.ino
+++ ESP-Firebase-Control-GPIOs-Anywhere/ESP-Firebase-Control-GPIOs-Anywhere.ino
@@ -1,5 +1,5 @@
 /*
-  Rui Santos
+  Rui Santos & Sara Santos - Random Nerd Tutorials
   Complete project details at our blog.
     - ESP32: https://RandomNerdTutorials.com/firebase-control-esp32-gpios/
     - ESP8266: https://RandomNerdTutorials.com/firebase-control-esp8266-nodemcu-gpios/
@@ -11,37 +11,43 @@
 
 #include <Arduino.h>
 #if defined(ESP32)
-  #include <WiFi.h>
+    #include <WiFi.h>
 #elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
+    #include <ESP8266WiFi.h>
 #endif
-#include <Firebase_ESP_Client.h>
+#include <WiFiClientSecure.h>
+#include <FirebaseClient.h>
+#include "ExampleFunctions.h" // Provides the functions used in the examples.
+#include <ArduinoJson.h>
 
-// Provide the token generation process info.
-#include "addons/TokenHelper.h"
-// Provide the RTDB payload printing info and other helper functions.
-#include "addons/RTDBHelper.h"
-
-// Insert your network credentials
+// Network and Firebase credentials
 #define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
 #define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
 
-// Insert Firebase project API Key
-#define API_KEY "REPLACE_WITH_YOUR_PROJECT_API_KEY"
+#define Web_API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
+#define DATABASE_URL "REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL"
+#define USER_EMAIL "REPLACE_WITH_FIREBASE_PROJECT_EMAIL_USER"
+#define USER_PASS "REPLACE_WITH_FIREBASE_PROJECT_USER_PASS"
 
-// Insert Authorized Username and Corresponding Password
-#define USER_EMAIL "REPLACE_WITH_THE_USER_EMAIL"
-#define USER_PASSWORD "REPLACE_WITH_THE_USER_PASSWORD"
+// User functions
+void processData(AsyncResult &aResult);
 
-// Insert RTDB URLefine the RTDB URL
-#define DATABASE_URL "REPLACE_WITH_YOUR_DATABASE_URL"
+// Authentication
+UserAuth user_auth(Web_API_KEY, USER_EMAIL, USER_PASS);
 
-// Define Firebase objects
-FirebaseData stream;
-FirebaseAuth auth;
-FirebaseConfig config;
+SSL_CLIENT ssl_client, stream_ssl_client;
 
-// Variables to save database paths
+// Firebase components
+FirebaseApp app;
+using AsyncClient = AsyncClientClass;
+AsyncClient aClient(ssl_client), streamClient(stream_ssl_client);
+RealtimeDatabase Database;
+
+// Timer variables for loop
+unsigned long lastSendTime = 0;
+const unsigned long sendInterval = 10000; // 10 seconds in milliseconds
+
+// Database  path (where the data is)
 String listenerPath = "board1/outputs/digital/";
 
 // Declare outputs
@@ -61,112 +67,129 @@ void initWiFi() {
   Serial.println();
 }
 
-// Callback function that runs on database changes
-void streamCallback(FirebaseStream data){
-  Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
-                data.streamPath().c_str(),
-                data.dataPath().c_str(),
-                data.dataType().c_str(),
-                data.eventType().c_str());
-  printResult(data); //see addons/RTDBHelper.h
-  Serial.println();
-
-  // Get the path that triggered the function
-  String streamPath = String(data.dataPath());
-
-  // if the data returned is an integer, there was a change on the GPIO state on the following path /{gpio_number}
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer){
-    String gpio = streamPath.substring(1);
-    int state = data.intData();
-    Serial.print("GPIO: ");
-    Serial.println(gpio);
-    Serial.print("STATE: ");
-    Serial.println(state);
-    digitalWrite(gpio.toInt(), state);
-  }
-
-  /* When it first runs, it is triggered on the root (/) path and returns a JSON with all keys
-  and values of that path. So, we can get all values from the database and updated the GPIO states*/
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json){
-    FirebaseJson json = data.to<FirebaseJson>();
-
-    // To iterate all values in Json object
-    size_t count = json.iteratorBegin();
-    Serial.println("\n---------");
-    for (size_t i = 0; i < count; i++){
-        FirebaseJson::IteratorValue value = json.valueAt(i);
-        int gpio = value.key.toInt();
-        int state = value.value.toInt();
-        Serial.print("STATE: ");
-        Serial.println(state);
-        Serial.print("GPIO:");
-        Serial.println(gpio);
-        digitalWrite(gpio, state);
-        Serial.printf("Name: %s, Value: %s, Type: %s\n", value.key.c_str(), value.value.c_str(), value.type == FirebaseJson::JSON_OBJECT ? "object" : "array");
-    }
-    Serial.println();
-    json.iteratorEnd(); // required for free the used memory in iteration (node data collection)
-  }
-  
-  //This is the size of stream payload received (current and max value)
-  //Max payload size is the payload size under the stream path since the stream connected
-  //and read once and will not update until stream reconnection takes place.
-  //This max value will be zero as no payload received in case of ESP8266 which
-  //BearSSL reserved Rx buffer size is less than the actual stream payload.
-  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
-}
-
-void streamTimeoutCallback(bool timeout){
-  if (timeout)
-    Serial.println("stream timeout, resuming...\n");
-  if (!stream.httpConnected())
-    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
-}
-
 void setup(){
   Serial.begin(115200);
-  initWiFi();
 
-  // Initialize Outputs
+  // Declare pins as outputs
   pinMode(output1, OUTPUT);
   pinMode(output2, OUTPUT);
   pinMode(output3, OUTPUT);
-  
-  // Assign the api key (required)
-  config.api_key = API_KEY;
 
-  // Assign the user sign in credentials
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
+  initWiFi();
 
-  // Assign the RTDB URL (required)
-  config.database_url = DATABASE_URL;
+  // Configure SSL client
+  ssl_client.setInsecure();
+  stream_ssl_client.setInsecure();
+  #if defined(ESP32)
+    ssl_client.setConnectionTimeout(1000);
+    ssl_client.setHandshakeTimeout(5);
+    stream_ssl_client.setConnectionTimeout(1000);
+    stream_ssl_client.setHandshakeTimeout(5);
+  #elif defined(ESP8266)
+    ssl_client.setTimeout(1000); // Set connection timeout
+    ssl_client.setBufferSizes(4096, 1024); // Set buffer sizes
+    stream_ssl_client.setTimeout(1000); // Set connection timeout
+    stream_ssl_client.setBufferSizes(4096, 1024); // Set buffer sizes
+  #endif
 
-  Firebase.reconnectWiFi(true);
+  // Initialize Firebase
+  initializeApp(aClient, app, getAuth(user_auth), processData, "🔐 authTask");
+  app.getApp<RealtimeDatabase>(Database);
+  Database.url(DATABASE_URL);
 
-  // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-
-  // Assign the maximum retry of token generation
-  config.max_token_generation_retry = 5;
-
-  // Initialize the library with the Firebase authen and config
-  Firebase.begin(&config, &auth);
-
-  // Streaming (whenever data changes on a path)
-  // Begin stream on a database path --> board1/outputs/digital
-  if (!Firebase.RTDB.beginStream(&stream, listenerPath.c_str()))
-    Serial.printf("stream begin error, %s\n\n", stream.errorReason().c_str());
-
-  // Assign a calback function to run when it detects changes on the database
-  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
-
-  delay(2000);
+  // Set a database listener
+  streamClient.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
+  Database.get(streamClient, listenerPath, processData, true /* SSE mode (HTTP Streaming) */, "streamTask");
 }
 
 void loop(){
-  if (Firebase.isTokenExpired()){
-    Firebase.refreshToken(&config);
-    Serial.println("Refresh token");
+  // Maintain authentication and async tasks
+  app.loop();
+
+  // Check if authentication is ready
+  if (app.ready()){
+    //Do nothing - everything works with callback functions
+    unsigned long currentTime = millis();
+    if (currentTime - lastSendTime >= sendInterval){
+      // Update the last send time
+      lastSendTime = currentTime;
+      Serial.printf("Program running for %lu\n", currentTime);        
+    }
+  }
+}
+
+void processData(AsyncResult &aResult){
+  // Exits when no result available when calling from the loop.
+  if (!aResult.isResult())
+    return;
+
+  if (aResult.isEvent()){
+    Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.eventLog().message().c_str(), aResult.eventLog().code());
+  }
+
+  if (aResult.isDebug()){
+    Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+  }
+
+  if (aResult.isError()){
+    Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+  }
+
+  // When it receives data from the database
+  if (aResult.available()){
+    RealtimeDatabaseResult &RTDB = aResult.to<RealtimeDatabaseResult>();
+    // we received data from the streaming client
+    if (RTDB.isStream()) {
+      Serial.println("----------------------------");
+      Firebase.printf("task: %s\n", aResult.uid().c_str());
+      Firebase.printf("event: %s\n", RTDB.event().c_str());
+      Firebase.printf("path: %s\n", RTDB.dataPath().c_str());
+      Firebase.printf("etag: %s\n", RTDB.ETag().c_str());
+      Firebase.printf("data: %s\n", RTDB.to<const char *>());
+      Firebase.printf("type: %d\n", RTDB.type());
+
+      // RTDB.type = 6 means the result is a JSON : https://github.com/mobizt/FirebaseClient/blob/main/resources/docs/realtime_database_result.md#--realtime_database_data_type-type
+      // You receive a JSON when you initialize the stream
+      if (RTDB.type() == 6) {
+        Serial.println(RTDB.to<String>());
+        // Parse JSON
+        DynamicJsonDocument doc(512);
+        DeserializationError error = deserializeJson(doc, RTDB.to<String>());
+        if (error) {
+          Serial.print("deserializeJson() failed: ");
+          Serial.println(error.c_str());
+          return;
+        }
+        // Iterate through JSON object
+        for (JsonPair kv : doc.as<JsonObject>()) {
+          int gpioPin = atoi(kv.key().c_str()); // Convert key (e.g., "12") to int
+          bool state = kv.value().as<bool>();
+          digitalWrite(gpioPin, state ? HIGH : LOW);
+        }
+      }
+
+      // RTDB.type() = 4 means the result is a boolean
+      // RTDB.type() = 1 means the result is an integer
+      // learn more here: https://github.com/mobizt/FirebaseClient/blob/main/resources/docs/realtime_database_result.md#--realtime_database_data_type-type
+      if (RTDB.type() == 4 || RTDB.type() == 1){
+        // get the GPIO number
+        int GPIO_number = RTDB.dataPath().substring(1).toInt();
+        bool state = RTDB.to<bool>();
+        digitalWrite(GPIO_number, state);
+        Serial.println("Updating GPIO State");
+      }
+
+      // The stream event from RealtimeDatabaseResult can be converted to values as following.
+      /*bool v1 = RTDB.to<bool>();
+      int v2 = RTDB.to<int>();
+      float v3 = RTDB.to<float>();
+      double v4 = RTDB.to<double>();
+      String v5 = RTDB.to<String>();
+      Serial.println(v5); */
+    }
+    else{
+        Serial.println("----------------------------");
+        Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
+    }
   }
 }
